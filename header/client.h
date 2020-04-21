7babb7df51d8e93fbe07f01e38bff36088cb00fd
Ahmed <01-131172-004@student.bahria.edu.pk>
2020-04-21 23:40:33 +0500

    Header done almost

--- header/client.h
+++ header/client.h
@@ -1,4 +1,21 @@
 #ifndef CLIENT_H_
 #define CLIENT_H_
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
+class Client
+{
+    int socket;
+    int valRead;
+    int port;
+
+    char buffer[1024];
+
+    struct sockaddr_in serverAddress;
+
+public:
+    Client();
+    bool sendMessage(const char* message);
+    const char* listenMessage();
+};
 #endif
--- header/server.h
+++ header/server.h
@@ -1,6 +1,19 @@
 #ifndef SERVER_H_
 #define SERVER_H_
+#include<sys/socket.h>
+#include<netinet/in.h>
 
+class Server
+{
+    int serverFd;
+    int socket;
+    int valRead;
+    int opt;
+    int addressLength;
+
+    char budd[1024];
+public:
+};
 
 
 #endif /*SERVER_H_*/
